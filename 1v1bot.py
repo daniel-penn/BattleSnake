56353351b1b41ea263e07bdd45018bd9577ceb39
Daniel Penn <danielpenn100@gmail.com>
2026-09-23 16:14:58 +0300

    Add 1v1bot

---
+++ 1v1bot.py
@@ -0,0 +1,7 @@
+"""
+Takes the next step in the longest possible path that leads back to our own tail
+(flood rules: enemies that could kill us expand 1 in every direction and
+everyone loses 1 tail slot).
+
+When hungry, eat when given free food.
+"""
